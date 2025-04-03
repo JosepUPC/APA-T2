5b68b5a92b73c0cbabf1f91695c357ae13dd226c
JosepUPC <github.jeb@gmail.com>
2025-04-03 15:38:49 +0200

    changes

---
+++ Clase/clase_0327.py
@@ -0,0 +1,13 @@
+"""
+File clase_0327.py
+definition: we create a class to store data inside a list
+    1- write the class without a father
+    2- create the constructor with the in arguments and their attributes
+    3- 
+
+"""
+
+
+class Vector:
+    def __init__(self, iterable):
+        self._vector = []
